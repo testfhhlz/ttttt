4e3b298e7613b6dd65c2a3a5e0e4d6c724394933
root <625154402@qq.com>
2019-02-16 16:18:12 +0800

    添加一个属性

--- src/main/java/com/oa/controller/Hello.java
+++ src/main/java/com/oa/controller/Hello.java
@@ -21,4 +21,13 @@ package com.oa.controller;
  * Version:  <br>
  */
 public class Hello {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
