1afe0019ac877964d539ab60b7a628d6fd12dd68
fuhao <422460589@qq.com>
2019-02-16 16:34:41 +0800

    sss

--- src/main/java/com/oa/controller/tser.java
+++ src/main/java/com/oa/controller/tser.java
@@ -3,6 +3,9 @@ package com.oa.controller;
 public class tser {
 
     public static void main(String[] args) {
+
+        System.out.println("gittest    付豪提交");
+
         System.out.println("21222");
     }
 }
