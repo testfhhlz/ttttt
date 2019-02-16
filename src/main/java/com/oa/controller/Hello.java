b227c289a59e909e64460b927a2bd13279da23b6
root <625154402@qq.com>
2019-02-16 15:36:36 +0800

    IEDA整合测试GitHub

---
+++ src/main/java/com/oa/controller/Hello.java
@@ -0,0 +1,24 @@
+/**
+ * System Name： SDN Platform
+ * <p>
+ * File Name： Hello
+ * <p>
+ * Creating Time： 2019-02-16 15:35
+ * <p>
+ * Copyright (c) 2015-2025 Fiberhome Technologies.
+ * 88,YouKeYuan Road, Hongshan District.,Wuhan,P.R.China,
+ * Wuhan Research Institute of Post & Telecommunication.
+ * <p>
+ * All rights reserved.
+ */
+
+package com.oa.controller;
+
+/**
+ * Function Description:  <br>
+ * Writer: Administrator . <br>
+ * Creating Time: 2019-02-16 15:35 <br>
+ * Version:  <br>
+ */
+public class Hello {
+}
